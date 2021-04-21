8c553b4358122e4fb3cce142add1b21e6f1a132a
David Caicedo <davidfcc7@gmail.com>
2021-04-21 16:46:38 -0500

    comparaciones en javascript

---
+++ comparaciones/code.js
@@ -0,0 +1,8 @@
+// el signo = se utiliza para asignar un valor
+var a = 1, b = '1'
+
+// el signo == compara dos valores sin importar el tipo de dato y los iguala
+a == b //true ya que 1 en numero es igual al valor del string 1
+
+// el signo === compara el tipo de dos valores en este caso numero y string
+a === b //false ya que el tipo de a no el igual al tipo de b
