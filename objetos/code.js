ea3c1fbf05c363db8a98b88650a02f95614f856c
David Caicedo <davidfcc7@gmail.com>
2021-04-21 13:01:50 -0500

    creacion js

--- objetos/code.js
+++ objetos/code.js
@@ -2,15 +2,12 @@ var persona = {
     nombre: 'David',
     apellido: 'caicedo',
     edad: 28,
-    estatura: 192,
-    peso: 93
 }
 
 function nombrePersona (usuario){
-    var nombre = usuario.nombre
+    //var nombre = usuario.nombre
     // O
-    var {nombre} = usuario
-    console.log(nombre)
+    var {nombre,apellido,edad} = usuario
+    console.log(`Hola mi nombre es ${nombre} ${apellido} y tengo ${edad} años`)
 }
-
 nombrePersona(persona)
