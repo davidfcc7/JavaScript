3e7402f3e264be6466dd1efcf73f2776ee70a679
David Caicedo <davidfcc7@gmail.com>
2021-04-25 16:18:30 -0500

    estructura for, while, do while y switch case

---
+++ switchCase/code.js
@@ -0,0 +1,12 @@
+var signoZodiacal = prompt('¿cual es tu signo zodiacal?')
+
+switch(signoZodiacal){
+    case 'acuario':
+        console.log('tu signo es acuario')
+        break
+    case 'aries':
+        console.log('tu signo es aries')
+        break
+    default:
+        console.log('tu signo no existe eres un alien')
+}
