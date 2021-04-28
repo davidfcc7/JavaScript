8df4bf302f6b9a583c54eb9ddd6e807cfa182da2
David Caicedo <davidfcc7@gmail.com>
2021-04-28 11:56:17 -0500

    transformar un array

---
+++ transformarArray/code.js
@@ -0,0 +1,55 @@
+var david = {
+    nombre: 'david',
+    apellido: 'caicedo',
+    altura: 1.92
+}
+
+var lucia = {
+    nombre: 'lucia',
+    apellido: 'caicedo',
+    altura: 1.58
+}
+
+var sixto = {
+    nombre: 'sixto',
+    apellido: 'caicedo',
+    altura: 1.70
+}
+
+var gilma = {
+    nombre: 'gilma',
+    apellido: 'canchon',
+    altura: 1.68
+}
+//esto es una funcion flecha que retorna un valor
+
+//para mostrar todos los objetos dentro de un arreglo
+//los arreglos pueden ser de cualquier tipo de dato
+var familia = [david, lucia, sixto, gilma]
+
+//filtrar objetos de un array por elementos (una funcion declarada)
+var mtsAcms = familia.map(function(persona){
+    persona.altura *= 100
+    return persona.altura
+})
+
+var personasAltas = familia.filter(function (persona){
+    return persona.altura >= 170
+})
+
+var personasBajas = familia.filter(function({altura}){
+    return altura < 170
+})
+
+//mostrar todos los objetos de un arreglo
+console.log(familia)
+
+//mostrar los elementos de un solo objeto segun su posicion
+console.log(familia[0])
+
+//mostrar un elemento de un objeto que se encuentra dentro de un array
+console.log(familia[0].altura)
+console.log(familia[0]['nombre'])
+
+console.log(personasAltas)
+console.log(personasBajas)
