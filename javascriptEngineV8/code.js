27d81f5a9a0f70608f0b58c4532272179609916f
David Caicedo <davidfcc7@gmail.com>
2021-05-04 16:13:32 -0500

    hoisting

---
+++ javascriptEngineV8/code.js
@@ -0,0 +1,3 @@
+//window hace referencia a la pagina que se abre al entrar al archivo html
+// this = window (objeto global del entorno global)
+// hoisting = el orden en como se ejecuta un programa en java script
