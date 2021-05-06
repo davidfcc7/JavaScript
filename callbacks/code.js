59b9bcc9edde3ac03e73dbf52ff6c88eb331565d
David Caicedo <davidfcc7@gmail.com>
2021-05-06 18:33:20 -0500

    asincronismo

--- callbacks/code.js
+++ callbacks/code.js
@@ -7,23 +7,33 @@ const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id' // variable constante = url del campo (igual como esta en la API) / id del campo
 const PLANETS_URL = 'planets/:id'
 const STARSHIPS = 'starships/:id'
+const security = {crossDomain: true}
+
+const funcionPersonaje = function (date) {
+    console.log(`Hola yo soy ${date.name}, mido ${date.height} cm y peso ${date.mass} kg`)//la palabra reservada arguments contiene todos los argumentos de la funcion anterior
+}
+
+const funcionPlaneta = function (date){
+    console.log(`Vengo del planeta ${date.name} que tiene un clima ${date.climate}`)
+}
+
+const funcionNave = function (date){
+    console.log(`Estoy abordo de ${date.name} modelo ${date.model} con una capacidad de ${date.passengers} pasajeros`)
+}
 
 //3. hacer request (solicitar datos)
 
 //4. callback -> es una funcion que sera ejecutada a futuro y la llama "$.get"
 
-$.get(`${API_URL}${PEOPLE_URL.replace(':id', 1)}`, {crossDomain: true}, function (date) {
-    console.log(`Hola yo soy ${date.name}, mido ${date.height} cm y peso ${date.mass} kg`)//la palabra reservada arguments contiene todos los argumentos de la funcion anterior
-}) //obtener la url de la API y del primer campo, reemplazar el string ':id' por el id que deseamos obtener del campo PEOPLE_URL, crossDomain: true (colocar sistema de seguridad https para evitar suplantaciones)
-$.get(`${API_URL}${PLANETS_URL.replace(':id', 3)}`, {crossDomain: true}, function (date){
-    console.log(`Vengo del planeta ${date.name} que tiene un clima ${date.climate}`)
-})
+function obtenerPersonajes (idPersonaje, idPlaneta, idNave){
+    //obtener la url de la API y del primer campo, reemplazar el string ':id' por el id que deseamos obtener del campo PEOPLE_URL, crossDomain: true (colocar sistema de seguridad https para evitar suplantaciones)
+    const urlPeople = `${API_URL}${PEOPLE_URL.replace(':id', idPersonaje)}` 
+    const urlPlaneta = `${API_URL}${PLANETS_URL.replace(':id', idPlaneta)}`
+    const urlNave = `${API_URL}${STARSHIPS.replace(':id', idNave)}`
 
-const URL_SHIP = `${API_URL}${STARSHIPS.replace(':id', 9)}`
-const SECURITY = {crossDomain: true}
-const SHIP = function (date){
-    console.log(`Estoy abordo de ${date.name} modelo ${date.model} con una capacidad de ${date.passengers} pasajeros`)
+    $.get(urlPeople, security, funcionPersonaje)
+    $.get(urlPlaneta, security, funcionPlaneta)
+    $.get(urlNave, security, funcionNave)
 }
 
-$.get(URL_SHIP, SECURITY, SHIP)
-
+obtenerPersonajes(1, 1, 3)
