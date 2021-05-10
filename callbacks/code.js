3f97c6f5f500e1a590b567891b0328beac1f5f64
David Caicedo <davidfcc7@gmail.com>
2021-05-10 12:03:22 -0500

    promesas en paralelo

--- callbacks/code.js
+++ callbacks/code.js
@@ -34,32 +34,55 @@ function obtenerPersonajes(idPersonaje) {
     return new Promise(function (resolve, reject) { //funcion funciona o no funciona
         const urlPeople = `${API_URL}${PEOPLE_URL.replace(':id', idPersonaje)}`
         $.get(urlPeople, security, (datos) => resolve(datos)) //si funciona
-        .fail(() => reject(idPersonaje)) //si no funciona rechaza al personaje y muestra un error
+            .fail(() => reject(idPersonaje)) //si no funciona rechaza al personaje y muestra un error
     })
 }
 function obtenerPlaneta(idPlaneta) {
-    return new Promise(function(resolve, reject){
+    return new Promise(function (resolve, reject) {
         const urlPlaneta = `${API_URL}${PLANETS_URL.replace(':id', idPlaneta)}`
-        $.get(urlPlaneta, security, (datos)=>resolve(datos))
-        .fail(()=> reject(idPlaneta))//fail obtienen un callback que imprime un error en cado de no existir el dato o perder conexion con la api
+        $.get(urlPlaneta, security, (datos) => resolve(datos))
+            .fail(() => reject(idPlaneta))//fail obtienen un callback que imprime un error en cado de no existir el dato o perder conexion con la api
     })
 }
 function obtenerNave(idNave) {
-    return new Promise(function(resolve, reject){
+    return new Promise(function (resolve, reject) {
         const urlNave = `${API_URL}${STARSHIPS.replace(':id', idNave)}`
-        $.get(urlNave,security,(dato)=>resolve(dato))
-        .fail(()=>reject(idNave))
+        $.get(urlNave, security, (dato) => resolve(dato))
+            .fail(() => reject(idNave))
     })
 }
 
-let error = ()=> console.log(`error, dato no encontrado`)
+let error = () => console.log(`error, dato no encontrado`)
 
-obtenerPersonajes(1)
+var ids = [1]
+var promesasPersonaje = ids.map((idPersonaje) => obtenerPersonajes(idPersonaje))
+Promise
+    .all(promesasPersonaje)
+    .then((date) => console.log(date))
+    .catch(error)
+
+var ids = [1]
+var promesasPlaneta = ids.map((idPlaneta) => obtenerPlaneta(idPlaneta))
+Promise
+    .all(promesasPlaneta)
+    .then((date) => console.log(date))
+    .catch(error)
+
+var ids = [22]
+var promesasNaves = ids.map((idNave) => obtenerNave(idNave))
+Promise
+    .all(promesasNaves)
+    .then((date) => console.log(date))
+    .catch(error)
+/**
+ * CADENA DE PROMESAS EN SERIE
+ *
+ * obtenerPersonajes(1)
     .then((date) => {
         console.log(`Hola yo soy ${date.name}, mido ${date.height} cm y peso ${date.mass} kg`)
         return obtenerPlaneta(1)
     })
-    .catch(error) 
+    .catch(error)
     .then((date) => {
         console.log(`Vengo del planeta ${date.name}`)
         return obtenerNave(22)
@@ -69,3 +92,5 @@ obtenerPersonajes(1)
         console.log(`y estoy abordo del ${date.name}`)
     })
     .catch(error)
+ */
+
