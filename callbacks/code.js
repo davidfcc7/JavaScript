f573f6b5ef181d876743e7ef94b0fa815490fe14
David Caicedo <davidfcc7@gmail.com>
2021-05-10 09:31:34 -0500

    funcion callback e impresion asincrona

--- callbacks/code.js
+++ callbacks/code.js
@@ -9,31 +9,43 @@ const PLANETS_URL = 'planets/:id'
 const STARSHIPS = 'starships/:id'
 const security = {crossDomain: true}
 
-const funcionPersonaje = function (date) {
-    console.log(`Hola yo soy ${date.name}, mido ${date.height} cm y peso ${date.mass} kg`)//la palabra reservada arguments contiene todos los argumentos de la funcion anterior
-}
-
-const funcionPlaneta = function (date){
-    console.log(`Vengo del planeta ${date.name} que tiene un clima ${date.climate}`)
-}
-
-const funcionNave = function (date){
-    console.log(`Estoy abordo de ${date.name} modelo ${date.model} con una capacidad de ${date.passengers} pasajeros`)
-}
-
 //3. hacer request (solicitar datos)
 
 //4. callback -> es una funcion que sera ejecutada a futuro y la llama "$.get"
 
-function obtenerPersonajes (idPersonaje, idPlaneta, idNave){
+function obtenerPersonajes (idPersonaje, callback){
     //obtener la url de la API y del primer campo, reemplazar el string ':id' por el id que deseamos obtener del campo PEOPLE_URL, crossDomain: true (colocar sistema de seguridad https para evitar suplantaciones)
     const urlPeople = `${API_URL}${PEOPLE_URL.replace(':id', idPersonaje)}` 
+    $.get(urlPeople, security, function (date) {
+        console.log(`Hola yo soy ${date.name}, mido ${date.height} cm y peso ${date.mass} kg`)//la palabra reservada arguments contiene todos los argumentos de la funcion anterior
+        if (callback){
+            callback()
+        }
+    })
+}
+function obtenerPlaneta (idPlaneta, callback){
     const urlPlaneta = `${API_URL}${PLANETS_URL.replace(':id', idPlaneta)}`
+    $.get(urlPlaneta, security, function (date){
+        console.log(`Vengo del planeta ${date.name}`)
+        if (callback){
+            callback()
+        }
+    })
+}
+function obtenerNave (idNave, callback){
     const urlNave = `${API_URL}${STARSHIPS.replace(':id', idNave)}`
-
-    $.get(urlPeople, security, funcionPersonaje)
-    $.get(urlPlaneta, security, funcionPlaneta)
-    $.get(urlNave, security, funcionNave)
+    $.get(urlNave, security, function (date){
+        console.log(`Estoy abordo del ${date.name}`)
+        if (callback){
+            callback()
+        }
+    })
 }
 
-obtenerPersonajes(1, 1, 3)
+obtenerPersonajes(1, function(){
+    obtenerPlaneta(1, function(){
+        obtenerNave(22)
+    })
+})
+
+
