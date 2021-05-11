183e6eb8be56d55ff644d8da7abd738b1f98e94f
David Caicedo <davidfcc7@gmail.com>
2021-05-11 15:35:10 -0500

    constructor inicializar juego

--- juegoSimonDice/js/code.js
+++ juegoSimonDice/js/code.js
@@ -1,19 +1,33 @@
+//obtener los campos de cada color y el boton
 const celeste = document.getElementById('celeste')
 const violeta = document.getElementById('violeta')
 const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
 const btnEmpezar = document.getElementById('btnEmpezar')
 
+//clase juego
 class Juego {
     constructor() {
         this.inicializar()
+        this.generarSecuencia()
     }
-
+// cuando oprime el boton comenzar este desaparece
     inicializar() {
         btnEmpezar.classList.add('hide')
+        this.nivel = 1
+        this.colores = {
+            //si el objeto lleva el mismo nombre en la asignacion se puede escribir de ambas formas
+            celeste: celeste,
+            violeta: violeta, 
+            naranja,
+            verde
+        }
+    }
+    generarSecuencia(){
+        this.generarSecuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))//fill nos permite llenar un arreglo seleccionando la posicion inicial (0), map crea un array con los datos obtenidos en una funcion
     }
 }
-
+// funcion para empezar a jugar
 function empezarJuego() {
-    var juego = new Juego()
+    window.juego = new Juego()
 }
