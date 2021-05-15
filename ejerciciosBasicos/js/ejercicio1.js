bbdcc9366d8abfc72ab4cea196f2aed93478d2a5
David Caicedo <davidfcc7@gmail.com>
2021-05-15 14:00:20 -0500

    ejercicio uno de ejercicios basicps de Java Script

---
+++ ejerciciosBasicos/js/ejercicio1.js
@@ -0,0 +1,8 @@
+document.write('<h1 class="titulo">SUMA DE DOS NUMEROS</h1>')
+suma = (a, b) => {
+    let resultado = a + b
+    console.log(`Hola Mundo! el resultado de la suma es: ${resultado}`)
+}
+suma (3, 2)
+
+
