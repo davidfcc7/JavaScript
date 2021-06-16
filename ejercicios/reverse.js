c67822b1f00250f2c9891680767dc139d99d0b18
David Caicedo <davidfcc7@gmail.com>
2021-06-16 16:49:04 -0500

    reverse

--- ejercicios/reverse.js
+++ ejercicios/reverse.js
@@ -1,6 +1,6 @@
-let reverse = () => {
-    var array = [1,2,3,4,0]
-    array.reverse()
-    console.log(array)
-}
-reverse()
+var array = [1,2,3,4,0]
+    //array.reverse()
+    for (i = array.length; i >= 0; i--){
+        var value = array[i]
+        console.log(value)
+    }
